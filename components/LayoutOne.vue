d175994b0b39a3701a9a8bf5cfcd7bdab2f37e24
Florence Comajes <renztoygwapo@gmail.com>
2023-03-12 17:23:36 +0800

    added photo upload

--- components/LayoutOne.vue
+++ components/LayoutOne.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <button type="button" class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <button @click="fileOPen" type="button" class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             <svg class="mx-auto h-12 w-12 text-gray-400"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
             </svg>
             <span class="mt-2 block text-sm font-semibold text-gray-900">Photo 1</span>
+            <input type="file" id="file" style="display: none" @change="onSelectPhoto" />
             <p class="text-xs text-gray-400 mt-2">Browse Photo Here</p>
             </button>
     </div>
   </template>
+
+
+<script setup>
+import { ref } from 'vue' 
+
+const photo = ref('')
+const photoPath = ref('')
+
+const fileOPen = (async () => {
+    document.getElementById('file').click()
+})
+
+const onSelectPhoto = (async (e) => {
+    const file = e.target.files[0]
+    photo.value = file
+    photoPath.value = URL.createObjectURL(file)
+})
+
+</script>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,7 +3,7 @@
       <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex items-center gap-x-12">
           <div class="hidden lg:flex lg:gap-x-12">
-            <button v-for="item in navigation" :key="item.name" :href="item.href" @click="onSelectLayout(item.value)" class="rounded-md bg-indigo-200 py-2.5 px-3.5 text-sm text-white shadow-sm hover:bg-indigo-500 text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</button>
+            <button v-for="item in navigation" :key="item.name" :href="item.href" @click="onSelectLayout(item.value)" :class="item.value === util.layoutAction ? 'bg-indigo-500' : 'bg-indigo-200'" class="rounded-md py-2.5 px-3.5 text-sm text-white shadow-sm hover:bg-indigo-500 text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</button>
           </div>
         </div>
         <div class="flex lg:hidden">
@@ -20,10 +20,6 @@
         <div class="fixed inset-0 z-10" />
         <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
-              <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-            </a>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
               <span class="sr-only">Close menu</span>
               <XMarkIcon class="h-6 w-6" aria-hidden="true" />
@@ -32,7 +28,7 @@
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                <a v-for="item in navigation" :key="item.name" :href="item.href" @click="onSelectLayout(item.value)" :class="item.value === util.layoutAction ? 'bg-indigo-500' : 'bg-indigo-200'" class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
               </div>
               <div class="py-6">
                 <a href="#" class="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
